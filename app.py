cb1bab0b3ed8833b8e784e258f5660ecd796fa49
MCREVEL <mathis.crevel@viacesi.fr>
2024-02-04 17:48:43 +0100

    2

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask
+import flask
 import sqlite3
 import json
 import datetime
 import os
+
+
+
+app = flask.Flask(__name__, template_folder='views')
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
