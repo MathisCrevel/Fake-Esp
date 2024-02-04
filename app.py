139a62690124cdd5e51d7d132d443b2f101b6750
MCREVEL <mathis.crevel@viacesi.fr>
2024-02-04 18:03:56 +0100

    s

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 import flask
 import sqlite3
-import json
-import datetime
-import os
-
-connection = sqlite3.connect('data.db')
-
-cursor = connection.cursor()
-cursor.execute('CREATE TABLE IF NOT EXISTS dogs (id INTEGER PRIMARY KEY, name TEXT, age INTEGER, race TEXT)')
-connection.commit()
-connection.close()
-
 
 app = flask.Flask(__name__, template_folder='views')
 
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
-
+       return flask.render_template('index.html')
 
 @app.route('/add')
 def add():
